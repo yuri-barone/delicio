555de2ff190246336b092c94390ab9e100e032d2
yuri-barone <yuri-barone@hotmail.com>
2018-10-04 20:33:49 -0300

    changed imgs link

--- src/menuOptions.js
+++ src/menuOptions.js
@@ -11,7 +11,7 @@ export const foodItem = [
     {name:"Bolo de Chocolate", imgURL:"https://github.com/yuri-barone/delicio/blob/gh-pages/img/bolo-de-chocolate.jpg?raw=true", description:"Bolo de chocolate", price:"R$4.00", category:"sobremesas", recommended: true},
     {name:"Lasanha a Bolonhesa", imgURL:"https://github.com/yuri-barone/delicio/blob/gh-pages/img/lasanha-a-bolonhesa.jpg?raw=true", description:"Lasanha a Bolonhesa", price:"R$7.00", category:"lanches", recommended: true},
     {name:"Hambúrguer", imgURL:"https://github.com/yuri-barone/delicio/blob/gh-pages/img/hamburguer.jpg?raw=true", description:"Hámburgue de calabresa com bacon", price:"R$10.00", category:"lanches", recommended: true},
-    {name:"Pavê", imgURL:"https://github.com/yuri-barone/delicio/blob/gh-pages/img/pave-de-chocolate-branco.jpg?raw=true", description:"Pavê ou pa cumê", price:"R$7.00", category:"sobremesas", recommended:true},
+    {name:"Pavê", imgURL:"https://github.com/yuri-barone/delicio/blob/gh-pages/img/pave-chocolate-branco.jpg?raw=true", description:"Pavê ou pa cumê", price:"R$7.00", category:"sobremesas", recommended:true},
     {name:"Suco de laranja", imgURL:"https://github.com/yuri-barone/delicio/blob/gh-pages/img/suco-de-laranja.jpg?raw=true", description:"Suco de Laranja", category:"bebidas", price:"R$5.00", recommended: true}
     
 
